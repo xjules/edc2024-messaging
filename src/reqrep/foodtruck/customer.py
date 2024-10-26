51b563b8336de961a889cd0660243a3b4658354b
Julius Parulek <parulek@gmail.com>
2024-10-26 15:16:27 +0200

    Foodtruck example for rep req

---
+++ src/reqrep/foodtruck/customer.py
@@ -0,0 +1,32 @@
+import random
+import time
+
+import zmq
+
+
+def customer():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+    order_id = 0
+
+    while True:
+        # Create order
+        order = {"order_id": order_id, "item": "hotdog"}
+
+        print(f"Customer {order_id=}")
+        start_time = time.time()
+
+        # Send order and wait for response
+        socket.send_json(order)
+        response = socket.recv_json()
+
+        print(
+            f"Got order {response['order_id']} after {time.time() - start_time:.1f} seconds"
+        )
+        time.sleep(1)  # Wait before next order
+        order_id += 1
+
+
+if __name__ == "__main__":
+    customer()
---
+++ src/reqrep/foodtruck/foodtruck.py
@@ -0,0 +1,51 @@
+import time
+
+import zmq
+
+
+def take_order():
+    print("1. Taking the order...")
+    time.sleep(1)
+
+
+def get_ingredients():
+    print("2. Getting ingredients...")
+    time.sleep(2)
+
+
+def cook():
+    print("3. Cooking")
+    time.sleep(3)
+
+
+def pack_and_hand_over():
+    print("4. Packing and handing over")
+    time.sleep(1)
+
+
+def food_truck():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+
+    print("Food truck is ready!")
+
+    while True:
+        # Wait for order
+        order = socket.recv_json()
+        print(f"Received order: {order}")
+
+        take_order()
+
+        get_ingredients()
+
+        cook()
+
+        pack_and_hand_over()
+        # Send response
+        response = {"status": "ready", "order_id": order["order_id"]}
+        socket.send_json(response)
+
+
+if __name__ == "__main__":
+    food_truck()
